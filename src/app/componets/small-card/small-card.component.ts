1fdd32304a551b0fee0aca1726a5db99fe256966
Lucas <contatolucasirmao@gmail.com>
2023-10-18 13:51:47 -0300

    components dinamicos

--- src/app/componets/small-card/small-card.component.ts
+++ src/app/componets/small-card/small-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-small-card',
@@ -6,7 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./small-card.component.css']
 })
 export class SmallCardComponent implements OnInit {
-
+  @Input()
+  photoCover:string = "https://sm.ign.com/ign_br/lists/t/the-10-hig/the-10-highest-grossing-marvel-movies-of-all-time_n2xs.jpg"
+  @Input()
+  cardTitle: string = ""
   constructor() { }
 
   ngOnInit(): void {
